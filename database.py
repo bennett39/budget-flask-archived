1969aab34cdc1f35ff8fc6aadf0f793e4f36c82b
Bennett Garner <34491412+bennett39@users.noreply.github.com>
2018-10-28 00:15:24 +0000

    Fixed bug in balance update

--- database.py
+++ database.py
@@ -1,121 +1,6 @@
 from cs50 import SQL
 from flask import redirect, render_template, request, session
 
-def txs_test():
-    return {
-    'spHeader':{
-        'SP_HEADER_VERSION':-1,
-        'userStage':'B',
-        'betaTester':False,
-        'accountsMetaData':[
-            'HAS_ON_US',
-            'HAS_CASH',
-            'HAS_INVESTMENT',
-            'HAS_CREDIT'
-        ],
-        'success':True,
-        'qualifiedLead':False,
-        'developer':False,
-        'userGuid':'5689dc6f8d874eb59ad1052e162e3cd3',
-        'authLevel':'SESSION_AUTHENTICATED',
-        'deviceName':' #29',
-        'username':'bennettgarner@gmail.com',
-        'status':'ACTIVE'
-    },
-    'spData':{
-        'intervalType':'MONTH',
-        'endDate':'2018-10-25',
-        'moneyIn':10228.97,
-        'transactions':[
-            {
-                'symbol':'',
-                'isInterest':False,
-                'netCost':0.0,
-                'cusipNumber':'',
-                'accountName':'Blue Cash Everyday - X2005',
-                'description':'Wnyc/wqxr/njpr/nypr Xxxxxx4000 2xxx0804rgxxxxx-xx-04-rg--xxx8730',
-                'memo':'',
-                'isCredit':False,
-                'isEditable':True,
-                'isCashOut':True,
-                'merchantId':'1W4wINN6CUFA4v0N-xak17Y1DTRNstp-RdWdHZX1Ea4',
-                'price':0.0,
-                'holdingType':'',
-                'lotHandling':'',
-                'customReason':'',
-                'userTransactionId':5176324116,
-                'currency':'USD',
-                'isDuplicate':False,
-                'resultType':'aggregated',
-                'originalDescription':'Wnyc/wqxr/njpr/nypr Xxxxxx4000 2xxx0804rgxxxxx-xx-04-rg--xxx8730',
-                'isSpending':True,
-                'amount':5.0,
-                'checkNumber':'',
-                'transactionTypeId':0,
-                'isIncome':False,
-                'includeInCashManager':True,
-                'merchant':'',
-                'isNew':False,
-                'isCashIn':False,
-                'transactionDate':'2018-08-04',
-                'transactionType':'Debit',
-                'accountId':'10003703_12495704_48433256',
-                'originalAmount':5.0,
-                'isCost':False,
-                'userAccountId':48433256,
-                'simpleDescription':'Wnyc/wqxr/njpr/nypr Xx4000 2xx0804rgxx-04-rg--xx8730',
-                'catKeyword':'Wnyc Wqxr',
-                'runningBalance':0.0,
-                'hasViewed':False,
-                'categoryId':13,
-                'status':'posted'
-            },
-            {
-                'symbol':'',
-                'isInterest':False,
-                'netCost':0.0,
-                'cusipNumber':'',
-                'accountName':'Blue Cash Everyday - X2005',
-                'description':'Autopay Payment - Thank You',
-                'memo':'',
-                'isCredit':True,
-                'isEditable':True,
-                'isCashOut':False,
-                'merchantId':'zUrf4igY4ZMJgSedlLvH6b1vryI4_YkcbuRDxWVt-f8',
-                'price':0.0,
-                'holdingType':'',
-                'lotHandling':'',
-                'customReason':'',
-                'userTransactionId':5176324117,
-                'currency':'USD',
-                'isDuplicate':False,
-                'resultType':'aggregated',
-                'originalDescription':'Autopay Payment - Thank You',
-                'isSpending':False,
-                'amount':5.0,
-                'checkNumber':'',
-                'transactionTypeId':1,
-                'isIncome':False,
-                'includeInCashManager':False,
-                'merchant':'',
-                'isNew':False,
-                'isCashIn':True,
-                'transactionDate':'2018-08-05',
-                'transactionType':'Credit',
-                'accountId':'10003703_12495704_48433256',
-                'originalAmount':5.0,
-                'isCost':False,
-                'userAccountId':48433256,
-                'simpleDescription':'Autopay Payment - Thank You',
-                'catKeyword':'Payment Thank You',
-                'runningBalance':0.0,
-                'hasViewed':False,
-                'categoryId':51,
-                'status':'posted'
-            }
-        ]}
-    }
-
 def update_accounts(accounts, db):
     for i in accounts['spData']['accounts']:
         # Update institutions
@@ -144,8 +29,9 @@ def update_accounts(accounts, db):
                                  pc_accountid=i['accountId'])
 
         # Update balances
-        bal_id = db.execute("SELECT bal_id FROM balances WHERE time=:time", \
-                             time=i['lastRefreshed'])
+        bal_id = db.execute("SELECT bal_id FROM balances WHERE time=:time AND acc_id=:acc_id", \
+                             time=i['lastRefreshed'],
+                             acc_id=acc_id[0]['acc_id'])
         if not bal_id:
             db.execute("INSERT INTO balances (acc_id, balance, time) \
                         VALUES(:acc_id, :balance, :time)", \
