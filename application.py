c66265a8c0f1e576d043579526900839bc857baf
Bennett Garner <34491412+bennett39@users.noreply.github.com>
2018-11-03 13:12:28 +0000

    Revert changes

--- application.py
+++ application.py
@@ -41,7 +41,9 @@ app.config["SESSION_TYPE"] = "filesystem"
 Session(app)
 
 # Configure CS50 Library to use SQLite database
-db = SQL("postgres://dlztifkuqmpcoe:520e635dd80438cd1a53133cde676e8159cd9923b7c494fd3aa589d7ca49670d@ec2-107-20-249-48.compute-1.amazonaws.com:5432/d2levslkkchd80")
+db = SQL("sqlite:///budget.db")
+
+#postgres://dlztifkuqmpcoe:520e635dd80438cd1a53133cde676e8159cd9923b7c494fd3aa589d7ca49670d@ec2-107-20-249-48.compute-1.amazonaws.com:5432/d2levslkkchd80")
 
 # Configure API
 pc = PersonalCapital()
